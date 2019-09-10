824834ce90e93043c73fc5ff9fc67b2ffaa3aace
mehdi-irannezhad <Mehdi.loa@gmail.com>
2019-09-10 21:42:51 +0430

    non reachability is checked

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TargettedUnreachableJumpLabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You can see that \"Reached\" is not printed.");
+            try
+            {
+                Test();
+            }
+            catch
+            {
+            }
+            Console.ReadLine();
+        }
+
+        public static void Test()
+        {
+            try
+            {
+                goto label;
+            }
+            finally
+            {
+                throw new Exception();
+            }
+            label:
+                Console.WriteLine("Reached");
+        }
+    }
+}
